aaaef05f7bf549e2928f0ed81a3e3664f867bad7
BlackCat <62426860+blackcat-team@users.noreply.github.com>
2023-12-28 13:14:53 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-if [[ $EUID -ne 0 ]]; then
-   	echo "This script must be run as root"
-   	exit 1
-else
-	#Update and Upgrade
-	echo "Updating and Upgrading"
-	apt-get update && apt-get upgrade -y
-fi
-done
+apt update -y && apt install -y screen libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential git mc htop && screen
+git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git && cd ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh
+./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956#xnsub#xnsub -u RTnb2HH7yeYJEgWojwgQmUWTyoKRJf9MWd.blackcat -p x -t 3
