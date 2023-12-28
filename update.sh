0616b03886b76c5dd4c0890e6f2f9f6748e0cfb3
BlackCat <62426860+blackcat-team@users.noreply.github.com>
2023-12-28 13:34:59 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-apt update -y && apt install -y screen libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential git mc htop dos2unix && screen
-
-
-
-
+apt update -y && apt install -y screen libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential git mc htop dos2unix
+screen
 #./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956#xnsub#xnsub -u RTnb2HH7yeYJEgWojwgQmUWTyoKRJf9MWd.blackcat -p x -t 3
