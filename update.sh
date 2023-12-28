7a2ca318c2f6ee06c7586c2ed6a4134d5875fa2b
BlackCat <62426860+blackcat-team@users.noreply.github.com>
2023-12-28 14:02:44 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-apt update -y
-apt install -y screen libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential git mc htop 
-screen
-git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
-cd ccminer
-chmod +x build.sh
-chmod +x
-configure.sh
-chmod +x autogen.sh
-./build.sh
+apt update -y && apt install -y screen libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential git mc htop && screen
+git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git && cd ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh
